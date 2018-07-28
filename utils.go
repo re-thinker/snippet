84704f36e71225c98c1115c98b35ef7f999359e9
re-thinker <re-thinker@gmail.com>
2018-07-28 17:57:43 +0800

    rename params.go

--- utils.go
+++ utils.go
@@ -6,8 +6,8 @@ type MapStringInterface map[string]interface{}
 
 // StringToMap 转换字符串到map结构
 // 把XXX1=YYY;XX2=ZZZ 转换成map
-func StringToMap(params string) map[string]string  {
-	m := map[string]string{}
+func StringToMap(params string) MapStringInterface {
+	m := MapStringInterface{}
 	mapStrings := strings.Split(params, ";")
 	for _, mapWord := range mapStrings {
 		words := strings.Split(mapWord, "=")
